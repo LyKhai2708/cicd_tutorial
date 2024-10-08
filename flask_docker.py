7203a2c6326e9c6738b2ad00496976b0998e447d
Khai Ly <khaib2111930@student.ctu.edu.vn>
2024-10-08 13:05:36 +0700

    second commit

--- flask_docker.py
+++ flask_docker.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from werkzeug.urls import url_quote
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-	return 'Hello FOSS'
 
+
+def hello_world():
+    return 'Hello FOSS, Hello CI/CD using Jenkins'
 if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
+
+
